db00fee45564ad1b394230fcfb92243780099719
parya03 <pranit.arya03@gmail.com>
2023-07-20 13:59:54 -0500

    Adding configurability

--- nrf24l01.c
+++ nrf24l01.c
@@ -3,33 +3,38 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/spi.h>
 
-#define MAX_NRF_READ_SIZE 32
+#define MAX_NRF24_READ_SIZE 32
 
 // Global flag - contains current status
-// Sending command to nrf makes it shift out current status which can be stored here
-static uint8_t nrf_status;
+// Sending command to nrf24 makes it shift out current status which can be stored here
+static uint8_t nrf24_status;
+
+// Pointer to config structs for internal library use
+static nrf24_pin_config_t *nrf24_pin_config;
+static nrf24_config_t *nrf24_config;
 
 // Set CS pin
 static inline void set_cs(uint8_t status) {
-    while (!(SPI_SR(SPI5) & SPI_SR_TXE) || (SPI_SR(SPI5) & SPI_SR_BSY)); // Wait for transfer finished
+    while (!(SPI_SR(nrf24_pin_config->spi_periph) & SPI_SR_TXE) || (SPI_SR(nrf24_pin_config->spi_periph) & SPI_SR_BSY)); // Wait for transfer finished
     if(status) {
-        gpio_set(GPIOF, GPIO6);
+        gpio_set(nrf24_pin_config->csn_pin, nrf24_pin_config->csn_pin);
     }
     else {
-        gpio_clear(GPIOF, GPIO6);
+        gpio_clear(nrf24_pin_config->csn_pin, nrf24_pin_config->csn_pin);
     }
 }
 
 static inline void read_rx_fifo_until_empty(uint8_t *store_var, uint32_t max_size) {
     uint32_t count = 0;
-    while ((SPI_SR(SPI5) & SPI_SR_RXNE) && (count < max_size)) {
-        *(store_var + count) = spi_read(SPI5);
+    while ((SPI_SR(nrf24_pin_config->spi_periph) & SPI_SR_RXNE) && (count < max_size)) {
+        *(store_var + count) = spi_read(nrf24_pin_config->spi_periph);
         count++;
     }
 }
 
-static inline void wait_spi_not_busy() { // Includes checking busy bit, not just FIFO empty bit
-    while (!(SPI_SR(SPI5) & SPI_SR_TXE) || (SPI_SR(SPI5) & SPI_SR_BSY)); // Wait for transfer finished
+// Includes checking busy bit, not just FIFO empty bit
+static inline void wait_spi_not_busy() {
+    while (!(SPI_SR(nrf24_pin_config->spi_periph) & SPI_SR_TXE) || (SPI_SR(nrf24_pin_config->spi_periph) & SPI_SR_BSY)); // Wait for transfer finished
 }
 
 uint32_t write_mem(uint8_t address, uint8_t *data, uint32_t size) {
@@ -37,12 +42,12 @@ uint32_t write_mem(uint8_t address, uint8_t *data, uint32_t size) {
     set_cs(0);
     
     // Send instruction in format 001AAAAA where AAAAA is 5-bit address
-    spi_send(SPI5, ((address & 0x1F) | 0x20));
+    spi_send(nrf24_pin_config->spi_periph, ((address & 0x1F) | 0x20));
     wait_spi_not_busy();
-    nrf_status = spi_read(SPI5);
+    nrf24_status = spi_read(nrf24_pin_config->spi_periph);
     // Send data
     for(int i = 0; i < size; i++) {
-        spi_send(SPI5, *(data + i));
+        spi_send(nrf24_pin_config->spi_periph, *(data + i));
     }
 
     set_cs(1);
@@ -63,16 +68,16 @@ uint32_t write_reg_friendly(uint8_t address, uint8_t *data) {
 uint32_t read_mem(uint8_t address, uint8_t *data, uint32_t size) {
     
     set_cs(0);
-
+    
     // Send instruction in format 000AAAAA where AAAAA is 5-bit address
-    spi_send(SPI5, (address & 0x1F));
+    spi_send(nrf24_pin_config->spi_periph, (address & 0x1F));
     wait_spi_not_busy();
-    nrf_status = spi_read(SPI5);
+    nrf24_status = spi_read(nrf24_pin_config->spi_periph);
     // Read data
     for(int i = 0; i < size; i++) {
-        spi_send(SPI5, 0x00); // Keep clock clocking
+        spi_send(nrf24_pin_config->spi_periph, 0x00); // Keep clock clocking
         wait_spi_not_busy();
-        *(data + i) = spi_read(SPI5);
+        *(data + i) = spi_read(nrf24_pin_config->spi_periph);
     }
     
     set_cs(1);
@@ -80,39 +85,67 @@ uint32_t read_mem(uint8_t address, uint8_t *data, uint32_t size) {
     return 0;
 }
 
-uint32_t init_nrf(uint8_t rf_channel, nrf_role_t role, nrf_power_level_t power_level) {
+uint32_t init_nrf24(nrf24_pin_config_t *pin_config, nrf24_config_t *config) {
     // TODO: Other registers should be autoinit'd for testing, but maybe change later??
-    volatile uint8_t data = 0x0E + role;
-    write_mem(NRF_R_CONFIG, &data, 1);
+
+    nrf24_pin_config = pin_config;
+    nrf24_config = config;
+    
+    // Pin setup
+    // SPI
+	// Control CS# (PF6) in software, so not initialized as AF
+	gpio_mode_setup(pin_config->csn_port, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLDOWN, pin_config->csn_pin);
+	gpio_mode_setup(pin_config->ce_port, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, pin_config->ce_pin); // CE pin
+	gpio_set(pin_config->csn_port, pin_config->csn_pin); // Set CS high
+	gpio_set(pin_config->ce_port, pin_config->ce_pin); // Set CE high
+    gpio_set_output_options(pin_config->csn_port, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, pin_config->csn_pin);
+	spi_reset(pin_config->spi_periph);
+	spi_init_master(pin_config->spi_periph,
+		SPI_CR1_BAUDRATE_FPCLK_DIV_32,
+		SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+		SPI_CR1_CPHA_CLK_TRANSITION_1,
+		SPI_CR1_DFF_8BIT,
+		SPI_CR1_MSBFIRST);
+	// SPI_CR2(pin_config->spi_periph) |= SPI_CR2_SSOE;
+	// SPI_CR1(pin_config->spi_periph) = cr_tmp;
+	// spi_set_nss_high(pin_config->spi_periph);
+	spi_set_full_duplex_mode(pin_config->spi_periph);
+	// spi_enable_ss_output(pin_config->spi_periph);
+	// spi_enable_software_slave_management(pin_config->spi_periph);
+
+	spi_enable(pin_config->spi_periph);
+    
+    volatile uint8_t data = 0x0E + config->role;
+    write_mem(NRF24_R_CONFIG, &data, 1);
 
     // Verify power on/SPI communication
-    read_mem(NRF_R_CONFIG, &data, 1);
+    read_mem(NRF24_R_CONFIG, &data, 1);
     if(!(data & 0x02)) {
         return -1; // Not powered on or SPI error
     }
 
     data = 0x70;
-    write_mem(NRF_R_STATUS, &data, 1);
+    write_mem(NRF24_R_STATUS, &data, 1);
     data = 0x01;
-    write_mem(NRF_R_RX_PW_P0, &data, 1);
+    write_mem(NRF24_R_RX_PW_P0, &data, 1);
 
     // Set RF channel
-    data = (rf_channel & 0x7F);
-	write_mem(NRF_R_RF_CH, &data, 1);
+    data = (config->rf_channel & 0x7F);
+	write_mem(NRF24_R_RF_CH, &data, 1);
 
     // RF Setup
     data = 0x03;
-	write_mem(NRF_R_RF_SETUP, &data, 1);
+	write_mem(NRF24_R_RF_SETUP, &data, 1);
 
-    if(role == NRF_ROLE_RX) {
-        gpio_set(GPIOF, GPIO15); // Set CE high
+    if(config->role == NRF24_ROLE_RX) {
+        gpio_set(pin_config->ce_port, pin_config->ce_pin); // Set CE high
     }
-    if(role == NRF_ROLE_TX) {
-        gpio_clear(GPIOF, GPIO15); // Set CE low
+    if(config->role == NRF24_ROLE_TX) {
+        gpio_clear(pin_config->ce_port, pin_config->ce_pin); // Set CE low
         
         // Turn off auto ACK if TX role
         data = 0;
-        write_mem(NRF_R_EN_AA, &data, 1);
+        write_mem(NRF24_R_EN_AA, &data, 1);
     }
 
     return 0;
@@ -121,15 +154,15 @@ uint32_t init_nrf(uint8_t rf_channel, nrf_role_t role, nrf_power_level_t power_l
 uint32_t read_payload(uint8_t *recieveptr, uint32_t size) {
     set_cs(0);
 
-    spi_send(SPI5, 0x61); // Instruction for R_RX_PAYLOAD
+    spi_send(nrf24_pin_config->spi_periph, 0x61); // Instruction for R_RX_PAYLOAD
     wait_spi_not_busy();
-    nrf_status = spi_read(SPI5);
+    nrf24_status = spi_read(nrf24_pin_config->spi_periph);
 
     // Read data
     for(int i = 0; i < size; i++) {
-        spi_send(SPI5, 0x00); // Keep clock clocking
+        spi_send(nrf24_pin_config->spi_periph, 0x00); // Keep clock clocking
         wait_spi_not_busy();
-        *(recieveptr + i) = spi_read(SPI5);
+        *(recieveptr + i) = spi_read(nrf24_pin_config->spi_periph);
     }
 
     set_cs(1);
@@ -138,15 +171,15 @@ uint32_t read_payload(uint8_t *recieveptr, uint32_t size) {
 uint32_t write_payload(uint8_t *writeptr, uint32_t size) {
     set_cs(0);
 
-    spi_send(SPI5, 0xA0); // Instruction for W_TX_PAYLOAD
+    spi_send(nrf24_pin_config->spi_periph, 0xA0); // Instruction for W_TX_PAYLOAD
     wait_spi_not_busy();
-    nrf_status = spi_read(SPI5);
+    nrf24_status = spi_read(nrf24_pin_config->spi_periph);
 
     // Write data
     for(int i = 0; i < size; i++) {
-        spi_send(SPI5, writeptr[i]);
+        spi_send(nrf24_pin_config->spi_periph, writeptr[i]);
         wait_spi_not_busy();
-        // *(writeptr + i) = spi_read(SPI5);
+        // *(writeptr + i) = spi_read(nrf24_pin_config->spi_periph);
     }
 
     set_cs(1);
@@ -154,7 +187,7 @@ uint32_t write_payload(uint8_t *writeptr, uint32_t size) {
     wait_spi_not_busy();
 
     // Pulse CE to start transmission
-    gpio_set(GPIOF, GPIO15); // Set CE high
+    gpio_set(nrf24_pin_config->ce_port, nrf24_pin_config->ce_pin); // Set CE high
     for(volatile int i = 0; i < 180; i++);
-    gpio_clear(GPIOF, GPIO15); // Set CE low
+    gpio_clear(nrf24_pin_config->ce_port, nrf24_pin_config->ce_pin); // Set CE low
 }
--- nrf24l01.h
+++ nrf24l01.h
@@ -1,50 +1,77 @@
-#ifndef NRF_H
-#define NRF_H
+#ifndef NRF24_H
+#define NRF24_H
 
 #include <stdint.h>
 
 typedef enum {
-    NRF_ROLE_TX = 0,
-    NRF_ROLE_RX = 1
-} nrf_role_t;
+    NRF24_ROLE_TX = 0,
+    NRF24_ROLE_RX = 1
+} nrf24_role_t;
 
 typedef enum {
-    NRF_PWR_LOW = 0,
-    NRF_PWR_MED_1 = 1,
-    NRF_PWR_MED_2 = 2,
-    NRF_PWR_MAX = 3
-} nrf_power_level_t;
+    NRF24_PWR_MIN = 0,
+    NRF24_PWR_MED_1 = 1,
+    NRF24_PWR_MED_2 = 2,
+    NRF24_PWR_MAX = 3
+} nrf24_power_level_t;
+
+typedef enum {
+    NRF24_PIPE_0 = 0,
+    NRF24_PIPE_1 = 1,
+    NRF24_PIPE_2 = 2,
+    NRF24_PIPE_3 = 3,
+    NRF24_PIPE_4 = 4,
+    NRF24_PIPE_5 = 5
+} rx_pipe_t;
+
+typedef struct {
+    uint32_t spi_periph; // SPI1, SPI2, SPI3, etc
+    uint32_t spi_port; // GPIO port for SPI
+    uint32_t spi_pins; // GPIO pins for SPI bitwise OR (|)'d together
+    uint32_t csn_port; // GPIO port for CSN
+    uint32_t csn_pin; // GPIO pin for CSN
+    uint32_t ce_port; // GPIO port for CE
+    uint32_t ce_pin; // GPIO pin for CE
+} nrf24_pin_config_t;
+
+typedef struct {
+    nrf24_role_t role; // NRF24_ROLE_TX or NRF24_ROLE_RX
+    uint32_t rf_channel; // RF channel, 0x00 to 0x7F 
+    nrf24_power_level_t power_level; // Power level for LNA
+    uint8_t enable_auto_ack; // Enable auto ack TX/RX
+    rx_pipe_t rx_pipe; // TODO
+} nrf24_config_t;
 
 uint32_t write_mem(uint8_t address, uint8_t *data, uint32_t size);
 uint32_t read_mem(uint8_t address, uint8_t *data, uint32_t size);
-uint32_t init_nrf(uint8_t rf_channel, nrf_role_t role, nrf_power_level_t power_level);
+uint32_t init_nrf24(nrf24_pin_config_t *pin_config, nrf24_config_t *config);
 uint32_t read_payload(uint8_t *recieveptr, uint32_t size);
 uint32_t write_payload(uint8_t *writeptr, uint32_t size);
 
 // Register addresses:
-#define NRF_R_CONFIG 0x00
-#define NRF_R_EN_AA 0x01
-#define NRF_R_EN_RXADDR 0x02
-#define NRF_R_SETUP_AW 0x03
-#define NRF_R_SETUP_RETR 0x04
-#define NRF_R_RF_CH 0x05
-#define NRF_R_RF_SETUP 0x06
-#define NRF_R_STATUS 0x07
-#define NRF_R_OBSERVE_TX 0x08
-#define NRF_R_CD 0x09
-#define NRF_R_RX_ADDR_P0 0x0A
-#define NRF_R_RX_ADDR_P1 0x0B
-#define NRF_R_RX_ADDR_P2 0x0C
-#define NRF_R_RX_ADDR_P3 0x0D
-#define NRF_R_RX_ADDR_P4 0x0E
-#define NRF_R_RX_ADDR_P5 0x0F
-#define NRF_R_TX_ADDR 0x10
-#define NRF_R_RX_PW_P0 0x11
-#define NRF_R_RX_PW_P1 0x12
-#define NRF_R_RX_PW_P2 0x13
-#define NRF_R_RX_PW_P3 0x14
-#define NRF_R_RX_PW_P4 0x15
-#define NRF_R_RX_PW_P5 0x16
-#define NRF_R_FIFO_STATUS 0x17
+#define NRF24_R_CONFIG 0x00
+#define NRF24_R_EN_AA 0x01
+#define NRF24_R_EN_RXADDR 0x02
+#define NRF24_R_SETUP_AW 0x03
+#define NRF24_R_SETUP_RETR 0x04
+#define NRF24_R_RF_CH 0x05
+#define NRF24_R_RF_SETUP 0x06
+#define NRF24_R_STATUS 0x07
+#define NRF24_R_OBSERVE_TX 0x08
+#define NRF24_R_CD 0x09
+#define NRF24_R_RX_ADDR_P0 0x0A
+#define NRF24_R_RX_ADDR_P1 0x0B
+#define NRF24_R_RX_ADDR_P2 0x0C
+#define NRF24_R_RX_ADDR_P3 0x0D
+#define NRF24_R_RX_ADDR_P4 0x0E
+#define NRF24_R_RX_ADDR_P5 0x0F
+#define NRF24_R_TX_ADDR 0x10
+#define NRF24_R_RX_PW_P0 0x11
+#define NRF24_R_RX_PW_P1 0x12
+#define NRF24_R_RX_PW_P2 0x13
+#define NRF24_R_RX_PW_P3 0x14
+#define NRF24_R_RX_PW_P4 0x15
+#define NRF24_R_RX_PW_P5 0x16
+#define NRF24_R_FIFO_STATUS 0x17
 
 #endif
